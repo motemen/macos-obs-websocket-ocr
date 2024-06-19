c34e22107eba48609c8f4c57104035e90c66548b
Hironao OTSUBO <motemen@gmail.com>
2024-06-20 00:05:13 +0900

    cleanup

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,125 +2,175 @@ import Foundation
 import CoreGraphics
 import Vision
 import Vapor
+import struct os.OSAllocatedUnfairLock
 
-// RequestResponse
-// https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md#requestresponse-opcode-7
-// {
-//   "op": 7,
-//   "d": {
-//     "requestType": "SetCurrentProgramScene",
-//     "requestId": "f819dcf0-89cc-11eb-8f0e-382c4ac93b9c",
-//     "requestStatus": {
-//       "result": true,
-//       "code": 100
-//     }
-//   }
-// }
-
-
-struct OBSWebSocketMessage: Decodable {
-    let op: Int
-    let d: DataType
-
-    enum OpCode: Int {
-        case RequestResponse = 7
+func decodeMessagePayload(expectedOpCode: Int, expectedRequestType: String, data: Data, logger: Logger) -> (Any, [String: Any])? {
+    guard let obj = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+        logger.warning("Failed to parse JSON as dictionary: \(data)")
+        return nil
     }
 
-    enum DataType {
-        case requestResponse(RequestResponseData)
-        case unknown
+    guard let opCode = obj["op"] as? Int else {
+        logger.warning("Failed to get opCode: \(obj)")
+        return nil
     }
 
-    enum CodingKeys: String, CodingKey {
-        case op
-        case d
+    if opCode != expectedOpCode {
+        return nil
     }
 
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        op = try container.decode(Int.self, forKey: .op)
-        switch op {
-        case OpCode.RequestResponse.rawValue:
-            d = .requestResponse(try container.decode(RequestResponseData.self, forKey: .d))
-        default:
-            d = .unknown
+    guard let payload = obj["d"] as? [String: Any] else {
+        logger.warning("Failed to get payload: \(obj)")
+        return nil
+    }
+
+    if payload["requestType"] as? String != expectedRequestType {
+        return nil
+    }
+
+    guard let requestId = payload["requestId"] else {
+        logger.warning("Failed to get requestId: \(payload)")
+        return nil
+    }
+
+    return (requestId, payload)
+}
+
+func decodeGetSourceScreenshotResponseImageData(data: Data, logger: Logger) -> String? {
+    guard let (_, payload) = decodeMessagePayload(expectedOpCode: 7, expectedRequestType: "GetSourceScreenshot", data: data, logger: logger) else {
+        return nil
+    }
+
+    if let imageData = (payload["responseData"] as? [String: Any])?["imageData"] as? String {
+        return imageData
+    } else {
+        logger.warning("Failed to get imageData: \(payload)")
+        return .none
+    }
+}
+
+// Overview:
+// - Proxy WebSocket messages between client and upstream server
+// - On GetSourceScreenshot response, save latest image data
+// - On __GetTextFromLastScreenshot request, which is this proxy's custom request, recognize text from the latest image data
+func runOBSWebSocketProxySession(client: WebSocket, upstreamURL: String, on eventLoopGroup: EventLoopGroup, logger: Logger) {
+    // maxFrameSize defaults to 1<<14, which seems to be too small for OBS WebSocket message,
+    // especially for GetSourceScreenshot response.
+    // > [obs-websocket] [WebSocketServer::onClose] WebSocket client `[::1]:51344` has disconnected with code `1006` and reason: Underlying Transport Error
+    let config = WebSocketClient.Configuration.init(tlsConfiguration: nil, maxFrameSize: Int(UInt32.max))
+    _ = WebSocket.connect(to: upstreamURL, configuration: config, on: eventLoopGroup) { upstream in
+        let latestImageData = OSAllocatedUnfairLock(initialState: Optional<String>.none)
+
+        client.onText { ws, text in
+            logger.debug("--> \(text)")
+
+            if let (requestId, _) = decodeMessagePayload(expectedOpCode: 6, expectedRequestType: "__GetTextFromLastScreenshot", data: Data(text.utf8), logger: logger) {
+                latestImageData.withLock { state in
+                    var responseData = [:]
+                    defer {
+                        let responseJSON = [
+                            "op": 7,
+                            "d": [
+                                "requestType": "__GetTextFromLastScreenshot",
+                                "requestId": requestId,
+                                "requestStatus": ["result": true, "code": 100],
+                                "responseData": responseData as Any
+                            ]
+                        ] as [String: Any]
+                        client.send(try! JSONSerialization.data(withJSONObject: responseJSON))
+                    }
+
+                    guard let imageData = state else {
+                        logger.warning("No image data")
+                        return
+                    }
+
+                    let base64Data = Data(base64Encoded: String(imageData.dropFirst("data:image/png;base64,".count)))
+                    guard let dataProvider = CGDataProvider(data: base64Data! as CFData) else {
+                        logger.warning("Failed to create CGDataProvider")
+                        return
+                    }
+
+                    guard let cgImage = CGImage(pngDataProviderSource: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent) else {
+                        logger.warning("Failed to create CGImage")
+                        return
+                    }
+
+                    logger.debug("CGImage: \(cgImage)")
+
+                    var textResults = []
+
+                    let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+                    let request = VNRecognizeTextRequest { (request, error) in
+                        let observations = request.results as? [VNRecognizedTextObservation] ?? []
+
+                        for observation in observations {
+                            if let topCandidate = observation.topCandidates(1).first {
+                                logger.info("Recognized text: \(topCandidate.string)")
+                                logger.info("  at \(observation.boundingBox)")
+                                textResults.append([
+                                    "text": topCandidate.string,
+                                    "bounding_box": [
+                                        "x": observation.boundingBox.origin.x,
+                                        "y": observation.boundingBox.origin.y,
+                                        "width": observation.boundingBox.width,
+                                        "height": observation.boundingBox.height,
+                                    ]
+                                ])
+                            }
+                        }
+
+                        responseData = ["text_results": textResults]
+                    }
+
+                    request.recognitionLevel = .accurate
+                    request.automaticallyDetectsLanguage = true
+
+                    do {
+                        try requestHandler.perform([request])
+                    } catch {
+                        logger.error("Failed to perform text recognition: \(error)")
+                    }
+                }
+            } else {
+                upstream.send(text)
+            }
+        }
+
+        upstream.onText { ws, text in
+            // logger.debug("<-- \(text)")
+            client.send(text)
+
+            if let imageData = decodeGetSourceScreenshotResponseImageData(data: Data(text.utf8), logger: logger) {
+                latestImageData.withLock { state in
+                    state = imageData
+                }
+            }
+        }
+        
+        client.onClose.whenComplete { result in
+            switch result {
+            case .success:
+                logger.info("Client closed")
+            case .failure(let error):
+                logger.error("Client closed with error: \(error)")
+            }
+            _ = upstream.close()
+        }
+        upstream.onClose.whenComplete { result in
+            switch result {
+            case .success:
+                logger.info("Upstream closed")
+            case .failure(let error):
+                logger.error("Upstream closed with error: \(error)")
+            }
+            _ = client.close()
         }
     }
 }
-
-// OpCode 7
-struct RequestResponseData: Codable {
-    let requestType: String
-    let requestId: String // Int?
-    // let requestStatus: RequestStatus
-    let responseData: Optional<Dictionary<String, String>>
-}
-
 
 func routes(_ app: Application) throws {
     app.webSocket { req, client in
-        // maxFrameSize defaults to 1<<14, which seems to be too small for OBS WebSocket message,
-        // especially for GetSourceScreenshot response.
-        // > [obs-websocket] [WebSocketServer::onClose] WebSocket client `[::1]:51344` has disconnected with code `1006` and reason: Underlying Transport
-        let config = WebSocketClient.Configuration.init(tlsConfiguration: nil, maxFrameSize: Int(UInt32.max))
-        _ = WebSocket.connect(to: "ws://localhost:4455", configuration: config, on: req.eventLoop) { upstream in
-            client.onText { ws, text in
-                app.logger.debug("--> \(text)")
-                upstream.send(text)
-            }
-            upstream.onText { ws, text in
-                // app.logger.debug("<-- \(text)")
-                client.send(text)
-
-                // GenericMessageとしてデコード
-                do {
-                    let message = try JSONDecoder().decode(OBSWebSocketMessage.self, from: Data(text.utf8))
-                    if case let .requestResponse(data) = message.d {
-                        if data.requestType == "GetSourceScreenshot" {
-                            if let imageData = data.responseData?["imageData"] as? String {
-                                let base64Data = Data(base64Encoded: String(imageData.dropFirst("data:image/png;base64,".count)))
-                                if let dataProvider = CGDataProvider(data: base64Data! as CFData) {
-                                    if let cgImage = CGImage(pngDataProviderSource: dataProvider, decode: nil, shouldInterpolate: true, intent: .defaultIntent) {
-                                        app.logger.debug("CGImage: \(cgImage)")
-
-                                        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-                                        let request = VNRecognizeTextRequest { (request, error) in
-                                            let observations = request.results as? [VNRecognizedTextObservation] ?? []
-
-                                            for observation in observations {
-                                                if let topCandidate = observation.topCandidates(1).first {
-                                                    app.logger.info("Recognized text: \(topCandidate.string)")
-                                                    app.logger.info("  at \(observation.boundingBox)")
-                                                }
-                                            }
-                                        }
-
-                                        request.recognitionLevel = .accurate
-                                        request.automaticallyDetectsLanguage = true
-
-                                        do {
-                                            try requestHandler.perform([request])
-                                        } catch {
-                                            app.logger.error("Failed to perform text recognition: \(error)")
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } catch {
-                    app.logger.error("Error: \(error)")
-                }
-            }
-            
-            client.onClose.whenComplete { _ in
-                app.logger.info("Client closed")
-                _ = upstream.close()
-            }
-            upstream.onClose.whenComplete { result in
-                app.logger.info("Upstream closed \(result)")
-                _ = client.close()
-            }
-        }
+        runOBSWebSocketProxySession(client: client, upstreamURL: "ws://localhost:4455", on: req.eventLoop, logger: app.logger)
     }
 }
